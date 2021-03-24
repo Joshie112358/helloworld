87b4108d8b3ebf8ccee89ecf8106e8fc9799182f
Joshie112358 <joshuabarceinascabrera@yahoo.com.mx>
2021-03-24 16:23:27 -0600

    Finally working with flake

---
+++ src/LucidTemplates/HomeTemplate.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE NoImplicitPrelude    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+
+module LucidTemplates.HomeTemplate where
+
+import           Blaze.ByteString.Builder           (Builder)
+import qualified Blaze.ByteString.Builder           as Blaze
+import qualified Blaze.ByteString.Builder.Html.Utf8 as Blaze
+import           Data.Text.Lazy.IO                  as L
+import           Database.Persist.Sql               (fromSqlKey)
+import           Import                             hiding (Builder, Html, for_,
+                                                     toHtml, (==.))
+import           Lucid
+import qualified Lucid.Base                         as LucidBase
+import           Lucid.Supplemental
+import           System.IO                          (hSetEncoding, stdout, utf8)
+
+homePage :: Html ()
+homePage =  do
+  toHtmlRaw "<!-- Products -->"
+  section_ [ class_ "about full-screen d-lg-flex justify-content-center align-items-center", id_ "products" ] $ do
+    div_ [ class_ "container" ] $ do
+      p_ [] "Hola Mundo"
